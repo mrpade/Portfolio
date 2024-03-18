6f5323a89850cedfd9da5da7ac99596ff8311c8b
mrpade <ddjock12@gmail.com>
2024-03-18 09:50:13 +0100

    building up photography category and contacts dropdowns

--- portfolioWebsite.js
+++ portfolioWebsite.js
@@ -1,6 +1,13 @@
 let menuItem = document.querySelectorAll(".menu-item");
 let section = document.querySelectorAll(".section");
-let aboutMe = document.getElementById('about-me')
+let aboutMe = document.getElementById('about-me');
+let navBtn = document.querySelectorAll('.btn');
+
+let contacts = document.querySelector('.contacts');
+let contactsCloseBtn = document.querySelector('.close-btn');
+let contactsButton = document.querySelector('#contacts-btn');
+let socialsButton = document.querySelector('#socials-btn');
+let portfolioButton = document.querySelector('#portfolio-btn');
 
 menuItem.forEach((item, index) => {
   item.addEventListener("click", () => {
@@ -11,7 +18,37 @@ menuItem.forEach((item, index) => {
     aboutMe.style.transform = 'translateY(-100%)';
     aboutMe.style.display = 'none';
     
-    section[0].classList.add('section-active');
-
+    section.forEach(section => {
+      section.classList.remove('section-active')
+    })
+    let id = item.getAttribute('data-id');
+    let activeSection = Array.from(section).filter(section => {
+      return section.dataset.id === id
+    })
+    activeSection.forEach(section => {
+      section.classList.add('section-active');
+    })
   });
 });
+
+navBtn.forEach(element => {
+  element.addEventListener('click', () => {
+    navBtn.forEach(element => {
+      element.classList.remove('btn-active');
+    })
+    element.classList.add('btn-active');
+  })
+})
+
+contactsButton.addEventListener('click', ()=>{
+  contacts.style.opacity = '1';
+  contacts.style.visibility = 'visible';
+  contacts.style.transform = 'translateY(0)';
+})
+contactsCloseBtn.addEventListener('click', () => {
+  contacts.style.opacity = '0';
+  contacts.style.visibility = 'hidden';
+  contacts.style.transform = 'translateY(100%)';
+  contactsButton.classList.remove('btn-active');
+  portfolioButton.classList.add('btn-active');
+})
