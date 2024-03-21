deb5c6ee9cf078d1da56d15cc460f0a9b9f80499
mrpade <ddjock12@gmail.com>
2024-03-21 14:34:41 +0100

    Adding gallery to photo section

--- portfolioWebsite.js
+++ portfolioWebsite.js
@@ -12,6 +12,14 @@ let contactsInfo = document.querySelectorAll('.contacts p');
 let socials = document.querySelector(".socials");
 let socialsInfo = document.querySelectorAll('.socials p');
 
+let gallery = document.querySelector('.gallery');
+let galleryContainer = document.querySelector('.gallery-container');
+let galleryImage = document.querySelectorAll('.gallery-image');
+let previous = document.querySelector('#previous');
+let next = document.querySelector('#next');
+let photoCategory = document.querySelectorAll('.cat-card');
+let galleryCloseBtn = document.querySelector('#galleryCloseBtn');
+
 
 /*SWITCHING BETWEEN MENU SECTIONS*/
 menuItem.forEach((item, index) => {
@@ -99,4 +107,61 @@ contacts.addEventListener('mouseout', () => {
       contactsButton.classList.remove("btn-active");
       portfolioButton.classList.add("btn-active");
   }, 5000);
+})
+
+/*next.addEventListener('click', () => {
+  for (i = 0; i < galleryImage.length; i++){
+    galleryImage.forEach(element => {
+      element.classList.remove('gallery-image-active');
+    })
+    galleryImage[i].classList.add('gallery-image-active');
+  }
+})*/
+
+let i = 0;
+let last = galleryImage.length-1;
+console.log(last);
+
+next.addEventListener('click', () => {
+  previous.style.scale = '1';
+  i++;
+  console.log(i);
+  
+  galleryImage.forEach((element) => {
+    element.classList.remove('gallery-image-active');
+  })
+  galleryImage[i].classList.add('gallery-image-active');
+  if(i === last){
+    previous.style.scale = '0';
+    i = 0;
+    galleryImage.forEach((element) => {
+      element.classList.remove('gallery-image-active');
+    })
+    galleryImage[i].classList.add('gallery-image-active');
+  }
+})
+
+previous.addEventListener('click', () => {
+  i--;
+  console.log(i);
+
+  galleryImage.forEach(element => {
+    element.classList.remove('gallery-image-active');
+  })
+  galleryImage[i].classList.add('gallery-image-active');
+
+  if(i === 0){
+    previous.style.scale = '0';
+  }
+})
+
+photoCategory.forEach(element => {
+  element.addEventListener('click', () => {
+    gallery.style.display = 'block';
+    galleryContainer.style.boxShadow = '0 0 5px #a8a8a8';
+  })
+})
+
+galleryCloseBtn.addEventListener('click', () => {
+  gallery.style.display = 'none';
 })
